b6a052b20a47af17ed965b744c855f28de65fefb
Victor Jimenez <betabandido@gmail.com>
2018-05-05 21:19:00 +0200

    Add implementation for anomaly detection exercise

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -23,17 +23,25 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
+    predictions = pval < epsilon;
 
+    tp = sum(predictions == 1 & yval == 1);
+    fp = sum(predictions == 1 & yval == 0);
+    fn = sum(predictions == 0 & yval == 1);
 
+    if fp > 0
+        prec = tp / (tp + fp);
+    else
+        prec = 1;
+    end
 
+    if fn > 0
+        rec = tp / (tp + fn);
+    else
+        rec = 1;
+    end
 
-
-
-
-
-
-
-
+    F1 = 2 * prec * rec / (prec + rec);
 
     % =============================================================
 
